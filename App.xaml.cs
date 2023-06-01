33802a4a15a0784ebc89ef7d47e0642fd88f769f
musa-mudesir <102249874+musa-mudesir@users.noreply.github.com>
2023-06-01 17:00:12 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace phoneword;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace phoneword;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
